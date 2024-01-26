497ad4021c50aa420e6c4511688039b9e0f16302
NadetDev <nadetdev@gmail.com>
2024-01-26 22:14:26 +0100

    Tutorial model repository

---
+++ src/repositories/tutorial.repo.interface.ts
@@ -0,0 +1,13 @@
+import Tutorial from '../models/tutorial.model';
+
+interface ITutorialRepository {
+  save(tutorial: Tutorial): Promise<Tutorial>;
+  retrieveAll(searchParams: {
+    title: string;
+    published: boolean;
+  }): Promise<Tutorial[]>;
+  retreiveById(tutorialId: number): Promise<Tutorial | null>;
+  update(tutorial: Tutorial): Promise<number>;
+  delete(tutorialId: number): Promise<number>;
+  deleteAll(): Promise<number>;
+}
