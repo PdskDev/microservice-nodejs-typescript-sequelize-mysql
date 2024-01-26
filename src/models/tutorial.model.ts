df289d59b9a4dcc69c795512cff7efef58b2f0bd
NadetDev <nadetdev@gmail.com>
2024-01-26 21:50:21 +0100

    complete Tutorial model

--- src/models/tutorial.model.ts
+++ src/models/tutorial.model.ts
@@ -1,3 +1,32 @@
-import { Model } from 'sequelize-typescript';
+import { Column, DataType, Model, Table } from 'sequelize-typescript';
 
-export default class Tutorial extends Model {}
+@Table({
+  tableName: 'tutorials',
+})
+export default class Tutorial extends Model {
+  @Column({
+    type: DataType.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    field: 'id',
+  })
+  id?: number;
+
+  @Column({
+    type: DataType.STRING(255),
+    field: 'title',
+  })
+  title?: string;
+
+  @Column({
+    type: DataType.STRING(255),
+    field: 'description',
+  })
+  description?: string;
+
+  @Column({
+    type: DataType.BOOLEAN,
+    field: 'published',
+  })
+  published?: boolean;
+}
